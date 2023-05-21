98fd61c6e26b7c35f7a65996a16621887e8d4208
alexa <alexanderredar1@gmail.com>
2023-05-21 12:36:01 +0300

    [2] - Функциональные интерфейсы в Lambda выражениях.

---
+++ src/com/javacode/lambdas/LambdaExample.java
@@ -0,0 +1,71 @@
+package com.javacode.lambdas;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+@FunctionalInterface
+interface ElementProcessor<T extends Number> {
+    double process(T element);
+}
+
+@FunctionalInterface
+interface ExecutiveFunction{
+    void process();
+}
+
+public class LambdaExample {
+
+    public static void main(String[] args) {
+        List<Integer> intList = new ArrayList<>();
+        intList.add(1);
+        intList.add(2);
+        intList.add(3);
+        intList.add(4);
+        intList.add(5);
+        List<Double> doubleList = new ArrayList<>();
+        doubleList.add(6.4);
+        doubleList.add(8.6);
+        doubleList.add(1.23);
+        doubleList.add(4.13);
+        doubleList.add(12.2);
+
+        processElements(intList, (x)->Math.sin(x.doubleValue()));
+        processElements(doubleList, (x)->Math.sin(x.doubleValue()));
+
+        TimeUntil.measure(()->Arrays.sort(createRandomArray()));
+//       TimeUntil.measure(Arrays.sort(a));
+    }
+
+    private static <T extends Number> void processElements(List<T> intList, ElementProcessor function) {
+        List<Double> doubleList = new ArrayList<>();
+        for (Number i : intList) {
+            doubleList.add(function.process(i));
+        }
+        System.out.println(doubleList);
+    }
+
+    private static double multiply(int x, int y){
+        return x * y / 10.0;
+    }
+
+    private static int[] createRandomArray() {
+        int[] myArray = new int[1000000];
+        Random r = new Random();
+        for (int i = 0; i < myArray.length; i++){
+            myArray[i] = r.nextInt(myArray.length);
+        }
+        return myArray;
+    }
+
+    public static class TimeUntil{
+
+        private static void measure(ExecutiveFunction function) {
+            long start = System.currentTimeMillis();
+            function.process();
+            long end = System.currentTimeMillis();
+            System.out.println("Time spent "+(end-start));
+        }
+    }
+}
